d3ca2733e11764026eb7b3aca574f064f18f2708
maryam223 <maryam.traore@gmail.com>
2021-12-10 09:23:49 +0100

    Delete sum.ts

--- src/sum.ts
+++
@@ -1,8 +0,0 @@
-export default class MyCalculator {
-
-    // Coder une methode add qui prend 2 nombres en paramètre et renvoie la somme des deux
-    add(a: number, b: number) {
-      return a + b;
-    }
-  }
-  
